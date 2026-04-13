d4e1f738597f204061969f1d027968e14bb66e9b
PRAGATH.B <pragonrr2007@gmail.com>
2026-04-13 18:55:29 +0530

    Create SERVER.js

---
+++ SERVER.js
@@ -0,0 +1,11 @@
+{
+  "name": "jarvis-local",
+  "version": "1.0.0",
+  "description": "J.A.R.V.I.S Local AI Assistant ",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {}
+}
